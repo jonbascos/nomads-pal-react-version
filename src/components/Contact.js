d8a2c3a43b4eee2845464468c453341f3805e06c
Jon Bascos <jon@jonbascos.com>
2020-04-27 11:23:07 -0700

    Removed fake address.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import '../static/css/Contact.css';
+import React from "react";
+import "../static/css/Contact.css";
 
 function Contact(props) {
-	const twitter = 'https://www.twitter.com/nomadspal';
+	const twitter = "https://www.twitter.com/nomadspal";
 	return (
 		<div className='contact-container'>
 			<h4>Contact Us</h4>
-			Email:{' '}
+			Email:{" "}
 			<a
 				href='mailto:info@nomadspal.com'
 				target='_blank'
@@ -15,12 +15,6 @@ function Contact(props) {
 				info@nomadspal.com
 			</a>
 			<br />
-			<address>
-				Mailing Address:
-				<br /> PO Box 11323
-				<br />
-				Oregon City, OR 97045
-			</address>
 			<h5>Social Media</h5>
 			<a href={twitter} target='_blank' rel='noopener noreferrer'>
 				Twitter
